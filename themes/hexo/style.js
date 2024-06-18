72f7b2ef49f5faaa8b0d82a5eccfcc0f5c9db9a0
cut <77314023+nosgk@users.noreply.github.com>
2024-06-18 14:24:00 +0800

    Update style.js

--- themes/hexo/style.js
+++ themes/hexo/style.js
@@ -8,10 +8,10 @@ const Style = () => {
   return (<style jsx global>{`
     // 底色
     body{
-        background-color: #f5f5f5
+        background-color: #4A5CBA
     }
     .dark body{
-        background-color: black;
+        background-color: #11212D;
     }
   
     /*  菜单下划线动画 */
